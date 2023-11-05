109531bf2db2ffa6cdd59c75895c7b5a1b0374f0
Manu <uo275943@uniovi.es>
2023-11-05 18:04:35 +0100

    TEW-2324-2-UO275943-7.6-Servicios: Cliente Java

--- Cliente/appClientModule/Main.java
+++ Cliente/appClientModule/Main.java
@@ -2,12 +2,23 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
+import org.json.simple.*;
+
 public class Main {
 	public static void main(String[] args) {
 		Client client = ClientBuilder.newClient();
 		WebTarget target = client.target("http://localhost:8080/gestioneitorv4_0/pisos.json");
 		String result = target.request().get().readEntity(String.class);
 		System.out.println(result);
+		Object obj=JSONValue.parse(result);
+		JSONArray pisos = (JSONArray)obj;
+		//Imprimimos el contacto tercero (2) transformándolo a formato cadena.
+		System.out.println("----------- PISO ----------------");
+		System.out.println(pisos.get(2).toString());
+		System.out.println("----------- CIUDAD DE UN PISO ----------------");
+		JSONObject unPiso = (JSONObject) JSONValue.parse(pisos.get(2).toString());
+		String ciudad = (String)unPiso.get("Ciudad");
+		System.out.println(ciudad);
 	}
 
 	/* (non-Java-doc)
