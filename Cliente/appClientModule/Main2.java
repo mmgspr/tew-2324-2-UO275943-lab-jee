907cc6a1c69e9ede7fa04f7a042eb598240365eb
Manu <uo275943@uniovi.es>
2023-11-05 19:16:55 +0100

    TEW-2324-2-UO275943-7.7-Servicios: Cliente Java Ayuntamiento Gijón

--- Cliente/appClientModule/Main2.java
+++ Cliente/appClientModule/Main2.java
@@ -1,3 +1,8 @@
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
@@ -6,21 +11,30 @@ import org.json.simple.*;
 
 public class Main2 {
 	public static void main(String[] args) {
-		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://localhost:8080/gestioneitorv4_0/gijon.json");
-		String result = target.request().get().readEntity(String.class);
-		JSONObject obj = (JSONObject) JSONValue.parse(result);
-		JSONObject datos = (JSONObject) JSONValue.parse(obj.get("datos").toString());
-		JSONArray dato = (JSONArray) JSONValue.parse(datos.get("dato").toString());
-		System.out.println("----------- Dato 0 ----------------");
-		System.out.println(dato.get(0).toString());
-		System.out.println("----------- Dato 1 Por separado ----------------");
-		JSONObject dato1 = (JSONObject) JSONValue.parse(dato.get(1).toString());
-		System.out.println("id_equipo: " + dato1.get("id_equipo"));
-		System.out.println("id_tipo_equipo: "+dato1.get("id_tipo_equipo"));
-		System.out.println("ubicacion: "+dato1.get("ubicacion"));
-		System.out.println("fecha_instalacion: "+dato1.get("fecha_instalacion"));
-		System.out.println("fecha_consulta: "+dato1.get("fecha_consulta"));
+
+	    SSLContext sslContext;
+		try {
+			sslContext = SSLContext.getInstance("TLS");
+			sslContext.init(null, new TrustManager[]{new GestorCerts()}, null);
+		    Client client = ClientBuilder.newBuilder().sslContext(sslContext).build();
+			WebTarget target = client.target("https://opendata.gijon.es/descargar.php?id=941&tipo=JSON");
+			String result = target.request().get().readEntity(String.class);
+			JSONObject obj = (JSONObject) JSONValue.parse(result);
+			JSONObject datos = (JSONObject) JSONValue.parse(obj.get("datos").toString());
+			JSONArray dato = (JSONArray) JSONValue.parse(datos.get("dato").toString());
+			System.out.println("----------- Dato 0 ----------------");
+			System.out.println(dato.get(0).toString());
+			System.out.println("----------- Dato 1 Por separado ----------------");
+			JSONObject dato1 = (JSONObject) JSONValue.parse(dato.get(1).toString());
+			System.out.println("id_equipo: " + dato1.get("id_equipo"));
+			System.out.println("id_tipo_equipo: "+dato1.get("id_tipo_equipo"));
+			System.out.println("ubicacion: "+dato1.get("ubicacion"));
+			System.out.println("fecha_instalacion: "+dato1.get("fecha_instalacion"));
+			System.out.println("fecha_consulta: "+dato1.get("fecha_consulta"));
+		} catch (NoSuchAlgorithmException | KeyManagementException e1) {
+			e1.printStackTrace();
+		}
+
 	}
 
 	/* (non-Java-doc)
