205d0cba1776c0252e4bb10177e4cef54540b2df
UO275943 <UO275943@GI-AN-B6_09.aulasuo.uniovi.es>
2023-11-21 18:42:35 +0100

    TEW-2324-2-UO275943-9.1-Model: Método Edit alumno

---
+++ gestioneitorCliv0_0/WebContent/javascript/functions_crudalumno.js
@@ -0,0 +1,47 @@
+function Model() {
+	// Lista de alumnos.
+	this.tbAlumnos = null;
+
+	// Carga los datos del servicio sobreescribiendo el dato this.tbAlumnos.
+	this.load = function() {
+		this.tbAlumnos = AlumnosServicesRs.getAlumnos();
+	}
+	// Llamamos al servicio de alta de alumno
+	this.add = function(alumno) {
+		// Llamamos al servicio de alta de alumno
+		AlumnosServicesRs.saveAlumno({
+		    $entity : alumno,
+		    $contentType : "application/json"
+		});
+		// Recargamos la lista de alumnos.
+		this.load();
+	}
+	// Actualización de un alumno existente: PENDIENTE DE IMPLEMENTAR
+	this.edit = function(alumno) {
+		// Llamamos al servicio de alta de alumno
+		AlumnosServicesRs.updateAlumno({
+		    $entity : alumno,
+		    $contentType : "application/json"
+		});
+		// Recargamos la lista de alumnos.
+		this.load();
+	}
+	// Eliminación un alumno existente
+	this.remove = function(id_alumno) {
+		// Llamamos al servicio de borrado de alumno
+		AlumnosServicesRs.deleteAlumno({
+			id : id_alumno
+		});
+		// Recargamos la lista de alumnos.
+		this.load();
+	}
+	this.find = function(id_alumno) {
+		function checkAlumno(obj) {
+			return obj.id == id_alumno;
+		}
+		// Buscamos los datos del alumno cuyo id_alumno sea el mismo que el
+		// seleccionado
+		var alumno = this.tbAlumnos.find(checkAlumno);
+		return alumno;
+	}
+};
